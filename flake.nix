f5fe9a80f798c31b58ccbce901d0a6fa00069df2
Luna Heyman <luna.heyman@proton.me>
2024-09-07 19:12:33 +0200

    fix: use correct name for theme

--- flake.nix
+++ flake.nix
@@ -12,22 +12,22 @@
 
     package = { logo ? ./default-logo.gif }: pkgs.stdenv.mkDerivation {
       pname = "plymouth-gif-theme";
-      version = "1.0.0";
+      version = "1.0.1";
       dontBuild = true;
       src = ./.;
       nativeBuildInputs = [ pkgs.ffmpeg ];
       installPhase = ''
-        mkdir -p $out/share/plymouth/themes/custom-plymouth-theme/frames
-        pushd $out/share/plymouth/themes/custom-plymouth-theme/frames
+        mkdir -p $out/share/plymouth/themes/plymouth-gif-theme/frames
+        pushd $out/share/plymouth/themes/plymouth-gif-theme/frames
         ${pkgs.ffmpeg}/bin/ffmpeg -i ${logo} frame%d.png
         frame_count=$(ls . | wc -l)
         popd
 
-        mkdir -p $out/share/plymouth/themes/custom-plymouth-theme
-        cp -r * $out/share/plymouth/themes/custom-plymouth-theme
+        mkdir -p $out/share/plymouth/themes/plymouth-gif-theme
+        cp -r * $out/share/plymouth/themes/plymouth-gif-theme
         find $out/share/plymouth/themes/ -name \*.plymouth -exec sed -i "s@\/usr\/@$out\/@" {} \;
 
-        sed -i "1iFRAME_COUNT=$frame_count;" $out/share/plymouth/themes/custom-plymouth-theme/video.script
+        sed -i "1iFRAME_COUNT=$frame_count;" $out/share/plymouth/themes/plymouth-gif-theme/video.script
       '';
     };
   in
